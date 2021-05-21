51ce06ab9d7f12402beac4ba31c88f52c92d66cf
phc <58563331+phc1003@users.noreply.github.com>
2021-05-22 07:04:34 +0800

    Update X64_diy-part1.sh

--- X64_diy-part1.sh
+++ X64_diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
