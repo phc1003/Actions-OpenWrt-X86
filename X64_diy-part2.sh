c85bfcf8c0677b6218233acd8db25a827ab02f76
phc <58563331+phc1003@users.noreply.github.com>
2023-02-09 00:04:39 +0800

    Update X64_diy-part2.sh

--- X64_diy-part2.sh
+++ X64_diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.199.201/g' package/base-files/files/bin/config_generate
 
 rm -rf  feeds/luci/themes/luci-theme-argon
 
@@ -19,10 +19,6 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='X86'' package/lean/default-settings/files/zzz-default-settings
 
-sed -i "s/OpenWrt /OPWRT/g" package/lean/default-settings/files/zzz-default-settings
-
-sed -i 's/R23.2.14/Pro 23.2.14/g' package/lean/default-settings/files/zzz-default-settings
-
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
+sed -i "s/OpenWrt /OpenWrt/g" package/lean/default-settings/files/zzz-default-settings
 
 git clone --branch master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
